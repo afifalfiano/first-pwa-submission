1d9afcc7bc6a62028831ba79a0f7170b0d57fe1f
Afif Alfiano <afif.alfiano@uii.ac.id>
2020-10-12 16:07:57 +0700

    make responsive and readable code

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const elems = document.querySelectorAll('.sidenav');
   const quotesData = [
     'Benar di mata manusia belum tentu benar di mata Allah, begitu juga buruk di mata manusia belum tentu buruk di mata Allah',
@@ -18,93 +18,92 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(elems);
   loadNav();
 
-
-  function loadNav() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status != 200) return;
-
-        document.querySelectorAll('.topnav, .sidenav').forEach(
-            function(elm) {
-              elm.innerHTML = xhttp.responseText;
-            });
-
-        document.querySelectorAll('.sidenav a, .topnav a').forEach(
-            function(elm) {
-              elm.addEventListener('click', function(event) {
-                const sidenav = document.querySelector('.sidenav');
-                M.Sidenav.getInstance(sidenav).close();
-
-                page = event.target.getAttribute('href').substr(1);
-                loadPage(page);
-              });
-            },
-        );
-      }
-    };
-    xhttp.open('GET', 'nav.html', true);
-    xhttp.send();
-  }
-
   let page = window.location.hash.substr(1);
 
-  if (page == '') page = 'home';
+  if (page === '') page = 'home';
   loadPage(page);
-
-  function loadPage(page) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        const content = document.querySelector('#body-content');
-        const footer = document.querySelector('#footer-content');
-        if (this.status = 200) {
-          content.innerHTML = xhttp.responseText;
-          if (page != 'home' || page != '') {
-            footer.innerHTML = `
-          <style>
-          .page-footer {
-            padding-top: 0;
-            width: 100%;
-          }
-
-          </style>     
-        <footer class="page-footer  teal darken-3">
-        <div class="footer-copyright teal darken-3">
-          <div class="container">
-          © 2020 Copyright all reserved by Afif Alfiano
-          <a class="grey-text text-lighten-4 right" href="#home">Afif Alfiano</a>
-          </div>
-        </div>
-      </footer>
-            `;
-          }
-          if (page == 'home') {
-            const carousel = document.querySelectorAll('.carousel');
-            M.Carousel.init(carousel, {fullWidth: true, indicators: true});
-          } else if (page == 'project') {
-            const tootltip = document.querySelectorAll('.tooltipped');
-            M.Tooltip.init(tootltip, {position: 'top'});
-          } else if (page == 'about') {
-            const modal = document.querySelectorAll('.modal');
-            M.Modal.init(modal);
-            const generate = document.querySelector('.generate');
-            generate.addEventListener('click', function() {
-              const randomNumber = Math.floor(Math.random() * (quotesData.length));
-              document.getElementById('quotes').innerHTML = quotesData[randomNumber];
-            });
-          } else if (page == 'contact') {
-            const collapse = document.querySelectorAll('.collapsible');
-            M.Collapsible.init(collapse);
-          }
-        } else if (this.status == 404) {
-          content.innerHTML = '<p> Halaman tidak ditemukan.</p<';
-        } else {
-          content.innerHTML = '<p>Upsss... halaman tidak dapat diakses</p>';
-        }
-      }
-    };
-    xhttp.open('GET', 'pages/' + page + '.html', true);
-    xhttp.send();
-  }
 });
+
+function loadNav() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status !== 200) return;
+
+      document.querySelectorAll('.topnav, .sidenav').forEach(
+          (elm) => {
+            elm.innerHTML = xhttp.responseText;
+          });
+
+      document.querySelectorAll('.sidenav a, .topnav a').forEach(
+          (elm) => {
+            elm.addEventListener('click', function(event) {
+              const sidenav = document.querySelector('.sidenav');
+              M.Sidenav.getInstance(sidenav).close();
+
+              page = event.target.getAttribute('href').substr(1);
+              loadPage(page);
+            });
+          },
+      );
+    }
+  };
+  xhttp.open('GET', 'nav.html', true);
+  xhttp.send();
+}
+
+function loadPage(page) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      const content = document.querySelector('#body-content');
+      const footer = document.querySelector('#footer-content');
+      if (this.status = 200) {
+        content.innerHTML = xhttp.responseText;
+        if (page !== 'home' || page !== '') {
+          footer.innerHTML = `
+        <style>
+        .page-footer {
+          padding-top: 0;
+          width: 100%;
+        }
+
+        </style>     
+      <footer class="page-footer  teal darken-3">
+      <div class="footer-copyright teal darken-3">
+        <div class="container">
+        © 2020 Copyright all reserved by Afif Alfiano
+        <a class="grey-text text-lighten-4 right" id="name" href="#home">Afif Alfiano</a>
+        </div>
+      </div>
+    </footer>
+          `;
+        }
+        if (page === 'home') {
+          const carousel = document.querySelectorAll('.carousel');
+          M.Carousel.init(carousel, {fullWidth: true, indicators: true});
+        } else if (page === 'project') {
+          const tootltip = document.querySelectorAll('.tooltipped');
+          M.Tooltip.init(tootltip, {position: 'top'});
+        } else if (page === 'about') {
+          const modal = document.querySelectorAll('.modal');
+          M.Modal.init(modal);
+          const generate = document.querySelector('.generate');
+          generate.addEventListener('click', function() {
+            const randomNumber = Math.floor(Math.random() * (quotesData.length));
+            document.getElementById('quotes').innerHTML = quotesData[randomNumber];
+          });
+        } else if (page === 'contact') {
+          const collapse = document.querySelectorAll('.collapsible');
+          M.Collapsible.init(collapse);
+        }
+      } else if (this.status === 404) {
+        content.innerHTML = '<p> Halaman tidak ditemukan.</p<';
+      } else {
+        content.innerHTML = '<p>Upsss... halaman tidak dapat diakses</p>';
+      }
+    }
+  };
+  xhttp.open('GET', 'pages/' + page + '.html', true);
+  xhttp.send();
+}
